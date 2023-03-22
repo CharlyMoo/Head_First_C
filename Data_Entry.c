2f1a96d5fe714eb8152e43d2283f82eb1dde2ef9
CharlyMoo <charlymooayala@gmail.com>
2023-03-21 19:03:32 -0600

    fgets() use

--- Data_Entry.c
+++ Data_Entry.c
@@ -11,11 +11,26 @@ int main() {
     int age;
     printf("Enter your age: ");
     scanf("%i", &age); // use the & operator to get the address of the int
-    printf("%i\n", age); */
+    printf("%i\n", age); 
     char first_name[20];
     char last_name[20];
     printf("Enter first and last name: ");
     scanf("%19s %19s", first_name, last_name); // This reads the first name, then a space, then the second name
     printf("First: %s Last: %s\n",first_name, last_name); // first and last names are stored in separate arrays. 
+    printf("\n"); */
+
+     // **************       fgets()     ***********************************************
+    // NEXT  PROGRAM CRASHES BECAUSE scanf() WRITES DATA WAY BEYOND THE END OF THE SPACE ALLOCATED
+    //       TO THE FOOD ARRAY
+    /*  char food[5];
+        printf("Enter favorite food: ");
+        scanf("%s", food);
+        printf("Favorite food: %s\n", food); 
+    */ 
+    // An alternative to enter text data without the overflowing
+    char food[25];
+    printf("Enter favorite food: ");
+    fgets(food, sizeof(food),stdin);
+    printf("Favorite food is: %s",food);  
     return 0;
 }
