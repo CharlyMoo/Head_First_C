e34dd9bc3904a445569e6f34d7a4a07bac1afeb0
CharlyMoo <charlymooayala@gmail.com>
2023-11-09 12:47:59 -0600

    last change

--- CH2/three_card_monte.c
+++ CH2/three_card_monte.c
@@ -25,3 +25,17 @@ int main(){
     void stack_deck(char *cards){ This function is equivalent to the one above
 
     } */
+
+/*
+    tip: try to never set a simple char pointer to a string literal value like: 
+        char *s = "Some String";
+
+    it can compile, but at some point of the code you would try to change it, and it cause problems and you would not find this bug.
+    So, if you want to set a pointer to a literal, always make sure you use the const keyword:
+        const char *s = "some string";
+    
+    That way, if the compiler sees some code that tries to modify the string, it will give you a compile error:
+        s[0] = 'S';
+        monte.c: 7: error: assignment of read-only location
+    
+*/
